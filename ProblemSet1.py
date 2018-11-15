6e7f9eaaf5eec80de9cf663de9aa85e2e849117c
lingjiangj <44994488+lingjiangj@users.noreply.github.com>
2018-11-15 15:25:35 +0800

    Create ProblemSet1.py

---
+++ ProblemSet1.py
@@ -0,0 +1,21 @@
+# problem1_1
+
+def problem1_1():
+    print("Problem Set 1")
+    
+    
+# problem1_2
+
+def problem1_2(x,y):
+    sum = x+y
+    product = x*y
+    print(sum)
+    print(product)
+ 
+# problem1_3
+
+def problem1_3(n):
+    my_sum = 0
+    for i in range (1,n+1):
+        my_sum +=i
+    print(my_sum)
